217d16a1d5a756b85ab7f5355359a354b4df14b4
github username <github email address>
2020-04-26 14:34:24 -0600

    adds make_clinics and print_clinics methods

--- lib/wic_practice_walkthrough/scraper.rb
+++ lib/wic_practice_walkthrough/scraper.rb
@@ -1,13 +1,32 @@
 require 'pry'
 
 class Scraper
+
   def get_page
-    doc = Nokogiri::HTML(open("https://www.wicprograms.org/ci/co-denver"))
-    doc.css(".span12").each do |clinic_info|
+    Nokogiri::HTML(open("https://www.wicprograms.org/ci/co-denver"))
+  end
+
+  def get_clinics
+    self.get_page.css(".span12")
+  end
+
+  def make_clinics
+    self.get_clinics.each do |clinic_info|
       clinic = WicPracticeWalkthrough::Clinics.new
       clinic.title = clinic_info.css("h2").first.text
       clinic.url = clinic_info.css("a").attr("href").text
       clinic.address_phone = clinic_info.css("p")[0].text.strip.gsub("/r/n", "").strip
+    end
+  end
+
+  def print_clinics
+    self.make_clinics
+    WicPracticeWalkthrough::Clinics.all.each do |clinic|
+      if clinic.title && clinic.title != ""
+        puts "Clinic Name: #{clinic.title}"
+        puts "  Website: #{clinic.url}"
+        puts "  Address and Phone: #{clinic.address_phone}"
+      end
     end
   end
 end
